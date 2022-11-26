e2cc36dbe4ab6ba2d001913f83331f60abc88ebe
dipes47 <dipes94@gmail.com>
2022-11-26 10:04:27 +0530

    m

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,30 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  //const expenseDate = new Date(2021, 2, 28);
+  //const expenseTitle = "Car Insurance";
+  //const expenseAmount = 294.67;
+
+  
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      {/* <div>March 28th 2021</div> */}
+      {/* <div>{expenseDate.toISOString()}</div> */}
+      {/* <div>{props.date.toISOString()}</div> */}
+      {/* <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div> */}
+      <ExpenseDate date = {props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        {/* <h2>Car Insurance</h2> */}
+        <h2>{props.title}</h2>
+        {/* <h2>{expenseTitle}</h2> */}
+        {/* <div className="expense-item__price">$294.67</div> */}
+        {/* <div className="expense-item__price">${expenseAmount}</div> */}
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
